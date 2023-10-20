df2a4b2d24434f6b50a777db14a3f14fd533a451
tungkk13 <fairplayx1nd@gmail.com>
2023-10-20 10:24:04 +0700

    update animation

--- js/script.js
+++ js/script.js
@@ -1,16 +1,40 @@
-let buttonPressed = false;
+var btn = document.getElementById("main_button");
+var mainPage = document.getElementById("main_page");
+var body = document.body;
 
-document.getElementById('main_button').addEventListener('click', function (event) {
-    event.preventDefault();
-    buttonPressed = true;
-    const target = document.querySelector('#main_page');
-    if (target) {
-        target.scrollIntoView({ behavior: 'smooth' });
-    }
-});
+body.style.overflow = "hidden";
 
-window.addEventListener('scroll', function () {
-    if (!buttonPressed) {
-        window.scrollTo(0, 0);
+btn.onclick = function () {
+    body.style.overflow = "auto";
+    sessionStorage.setItem("allowScroll", "true");
+    mainPage.scrollIntoView({ behavior: "smooth" });
+
+    setTimeout(function () {
+        var h2Element = document.getElementById("loichuc");
+        h2Element.classList.add("visible");
+
+        setTimeout(function () {
+            var endElement = document.getElementById("end_type");
+            endElement.classList.add("visible");
+            var h2Element = document.getElementById("khoeanh");
+            h2Element.classList.add("visible");
+            var imgElements = document.querySelectorAll("img.fade-in, khoeanh");
+            for (var i = 0; i < imgElements.length; i++) {
+                imgElements[i].classList.add("visible");
+            }
+        }, 2500);
+    }, 500);
+}
+
+window.onload = function () {
+    if (sessionStorage.getItem("allowScroll") === "true") {
+        body.style.overflow = "auto";
+    } else {
+        body.style.overflow = "hidden";
     }
-});
+    window.scrollTo(0, 0);
+}
+
+window.onbeforeunload = function () {
+    sessionStorage.removeItem("allowScroll");
+}
